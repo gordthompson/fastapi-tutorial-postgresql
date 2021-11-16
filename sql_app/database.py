0625142c754b3e33511f7decf9dd4db349440b7d
Gord Thompson <gord@gordthompson.com>
2021-11-16 13:04:12 -0700

    Initial commit

---
+++ sql_app/database.py
@@ -0,0 +1,18 @@
+from sqlalchemy.engine import URL
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import declarative_base, sessionmaker
+
+SQLALCHEMY_DATABASE_URL = URL.create(
+    "postgresql+asyncpg",
+    username="scott",
+    password="tiger",
+    host="192.168.0.199",
+    database="test",
+)
+
+engine = create_async_engine(SQLALCHEMY_DATABASE_URL, echo=False)
+SessionLocal = sessionmaker(
+    engine, expire_on_commit=False, class_=AsyncSession
+)
+
+Base = declarative_base()
